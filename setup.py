5c15544e11308c6521d114c95fd10170115a4f02
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2023-04-17 10:53:15 +0200

    v0.3.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ shutil.rmtree(Path("dist"), ignore_errors=True)
 
 setup(
     name="dmtpy",
-    version="0.3.2",
+    version="0.3.3",
     author="SINTEF Ocean",
     description="Python runtime library for SIMOS based DMT models",
     url="https://github.com/SINTEF/dmtpy",
